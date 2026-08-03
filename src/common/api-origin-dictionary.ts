f835633f96fc1174b3a8dffd57651cd27f783506
Reinier John Avila <reinieravila@gmail.com>
2026-08-03 15:43:27 +0800

    Config: Optional *_API_ORIGIN

---
+++ src/common/api-origin-dictionary.ts
@@ -0,0 +1,11 @@
+export const AIR_API = {
+  staging: 'https://air.api.staging.air3.com',
+  sandbox: 'https://air.api.sandbox.air3.com',
+  production: 'https://air.api.air3.com',
+};
+
+export const MOCA_CHAIN_API = {
+  staging: 'https://api.staging.mocachain.org',
+  sandbox: 'https://api.sandbox.mocachain.org',
+  production: 'https://mocachain-mainnet.api.air3.com',
+};
